932871d50ed4f96a109be70a43a5b12bad5b6c61
Anna Boyko <boyko.anna.108@gmail.com>
2017-12-26 14:16:59 +0200

    Add deck shuffle

--- js/app.js
+++ js/app.js
@@ -1,19 +1,40 @@
 window.onload = function () {
+	var deck = new CardDeck(cardDeck);
+	deck.drawDeck();
+}	
 
-	var cardDeck = [
-		"images/1.jpg", 
-		"images/2.jpg", 
-		"images/3.jpg", 
-		"images/4.jpg", 
-		"images/5.jpg", 
-		"images/6.jpg", 
-		"images/1.jpg", 
-		"images/2.jpg", 
-		"images/3.jpg", 
-		"images/4.jpg", 
-		"images/5.jpg", 
-		"images/6.jpg"
-	];
+var cardDeck = [
+	"images/1.jpg", 
+	"images/2.jpg", 
+	"images/3.jpg", 
+	"images/4.jpg", 
+	"images/5.jpg", 
+	"images/6.jpg", 
+	"images/1.jpg", 
+	"images/2.jpg", 
+	"images/3.jpg", 
+	"images/4.jpg", 
+	"images/5.jpg", 
+	"images/6.jpg"
+];
+
+class CardDeck {
+	constructor(cardDeck){
+		this.cardDeck = this.shuffle(cardDeck);
+	};
+};
+
+CardDeck.prototype.shuffle = function (d) {
+	for(var j, x, i = d.length; i; j = parseInt(Math.random() * i), x = d[--i], d[i] = d[j], d[j] = x);
+	return d;
+};
+
+CardDeck.prototype.drawDeck = function (){
+	cardDeck.forEach(function(card, index, cardDeck){
+		var card = new Card(card);
+		card.drawCard();
+	})
+};		
 
 class Card {
 	constructor(frontImagePath){
@@ -28,8 +49,8 @@ Card.prototype.drawCard = function(){
 	var frontImage = document.createElement("img");
 	var backImage = document.createElement("img");
 
-	section.insertBefore(div, section.firstChild);
-	// section.appendChild(div);
+	// section.insertBefore(div, section.firstChild);
+	section.appendChild(div);
 	div.appendChild(frontImage);
 	div.appendChild(backImage);
 
@@ -39,13 +60,3 @@ Card.prototype.drawCard = function(){
 	backImage.src = this.backImagePath;
 	backImage.className = "back";
 };
-	
-function drawDeck(){
-	cardDeck.forEach(function(card, index, cardDeck){
-		var card = new Card(card);
-		card.drawCard();
-	})
-};		
-	
-drawDeck();
-}
